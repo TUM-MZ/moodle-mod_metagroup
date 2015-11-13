b8b5f90af61ff8a0a5d0e9e5a20e2dab32db3e02
zakaria <zakariatanim@gmail.com>
2015-11-13 16:28:05 +0100

    csv read error bug fixed. trim added

--- handle_csv.php
+++ handle_csv.php
@@ -7,7 +7,7 @@ require_once('./csv_form.php');
 require_once('./locallib.php');
 
 admin_externalpage_setup('handlecsv');
-
+echo $OUTPUT->header();
 $mform = new csv_form();
 if ($form_data = $mform->get_data()) {
     global $DB;
@@ -26,23 +26,19 @@ if ($form_data = $mform->get_data()) {
         print_error('csvemptyfile', 'error', $returnurl, $cir->get_error());
     }
 
-//    $columns = $cir->get_columns();
     $cir->init();
-    while ($line = $cir->next()) {
-        // parse every line by comma and check if there are 3 items exactly in each line
-        $pieces = execute($line, ',');
+    while ($pieces = $cir->next()) {
         if (count($pieces) != 3){
             print_error('csvfileerror', 'enrol_metagroup', $returnurl, $cir->get_error());
             continue;
         }
-
         // get course object and parent course object
-        $course = get_course_from_shortname($pieces[0]);
-        $parent_course = get_course_from_shortname($pieces[1]);
+        $course = get_course_from_shortname(trim($pieces[0]));
+        $parent_course = get_course_from_shortname(trim($pieces[1]));
 
         // if none of the objects are missing
         if($course and $parent_course){
-            $parent_course_group = groups_get_group_by_name($parent_course->id, $pieces[2]);
+            $parent_course_group = groups_get_group_by_name($parent_course->id, trim($pieces[2]));
             if ($parent_course_group){
                 $course_list[] = $course->id;       // add course id in list, for sync call
                 $enrol->add_instance($course, array('customint1'=>$parent_course->id, 'customint2'=>$parent_course_group));
@@ -54,11 +50,12 @@ if ($form_data = $mform->get_data()) {
     foreach ($course_list as $cid){
         enrol_metagroup_sync($cid);
     }
+    
 
     unset($content);
 }
 
-echo $OUTPUT->header();
+
 $mform->display();
 echo $OUTPUT->footer();
 die();
