c9bcb783cc923b0ad1da86491bf1048839e4f43b
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2021-11-03 16:29:15 +0100

    Texte (en/de) an Formaktualisierung anpassen

--- lang/en/enrol_metagroup.php
+++ lang/en/enrol_metagroup.php
@@ -17,38 +17,41 @@
 /**
  * Strings for component 'enrol_metagroup', language 'en'.
  *
- * @package    enrol_metagroup
- * @copyright  2010 onwards Petr Skoda  {@link http://skodak.org}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   enrol_metagroup
+ * @copyright 2010 onwards Petr Skoda {@link http://skodak.org}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['linkedcourse'] = 'Link course';
-$string['metagroup:config'] = 'Configure metagroup enrol instances';
+$string['linkedcourse']             = 'Link course';
+$string['metagroup:config']         = 'Configure metagroup enrol instances';
 $string['metagroup:selectaslinked'] = 'Select course as metagroup linked';
-$string['metagroup:unenrol'] = 'Unenrol suspended users';
-$string['nosyncroleids'] = 'Roles that are not synchronised';
-$string['nosyncroleids_desc'] = 'By default all course level role assignments are synchronised from parent to child courses. Roles that are selected here will not be included in the synchronisation process. The roles available for synchronisation will be updated in the next cron execution.';
-$string['pluginname'] = 'Course metagroup link';
-$string['pluginname_desc'] = 'Course metagroup link enrolment plugin synchronises enrolments and roles in two different courses.';
-$string['syncall'] = 'Synchronise all enrolled users';
-$string['syncall_desc'] = 'If enabled all enrolled users are synchronised even if they have no role in parent course, if disabled only users that have at least one synchronised role are enrolled in child course.';
-$string['handlecsv'] = 'Import from csv';
-$string['csvfile'] = 'CSV file';
-$string['import'] = 'Import';
-$string['next'] = 'Next';
+$string['metagroup:unenrol']        = 'Unenrol suspended users';
+$string['nosyncroleids']            = 'Roles that are not synchronised';
+$string['nosyncroleids_desc']       = 'By default all course level role assignments are synchronised from parent to child courses. Roles ' .
+    'that are selected here will not be included in the synchronisation process. The roles available for synchronisation will be updated ' .
+    'in the next cron execution.';
+$string['pluginname']               = 'Course metagroup link';
+$string['pluginname_desc']          = 'Course metagroup link enrolment plugin synchronises enrolments and roles in two different courses.';
+$string['syncall']                  = 'Synchronise all enrolled users';
+$string['syncall_desc']             = 'If enabled all enrolled users are synchronised even if they have no role in parent course, if ' .
+    'disabled only users that have at least one synchronised role are enrolled in child course.';
+$string['handlecsv']                = 'Import from csv';
+$string['csvfile']                  = 'CSV file';
+$string['import']                   = 'Import';
+$string['next']         = 'Next';
 $string['csvfileerror'] = 'Invalid csv file';
-$string['linkedgroup'] = 'Link group';
-$string['remarks'] = 'Remarks';
-$string['course'] = 'Course';
+$string['linkedgroup']  = 'Link group';
+$string['remarks']      = 'Remarks';
+$string['course']       = 'Course';
 
 $string['form:field_id']   = 'Course ID';
 $string['form:field_name'] = 'Course name';
-$string['form:group']      = 'Course name';
+$string['form:searchterm'] = 'Course search';
+$string['form:group']      = 'Course group';
 
-$string['format_error'] = 'Format error. 3 items per row';
-$string['course_error'] = 'Invalid course';
+$string['format_error']      = 'Format error. 3 items per row';
+$string['course_error']      = 'Invalid course';
 $string['link_course_error'] = 'Invalid link course';
-$string['link_group_error'] = 'Invalid link group';
-$string['existing_error'] = 'Enrolment already exists';
-$string['eligibile'] = 'Enrolment will be added';
-
+$string['link_group_error']  = 'Invalid link group';
+$string['existing_error']    = 'Enrolment already exists';
+$string['eligibile']         = 'Enrolment will be added';
