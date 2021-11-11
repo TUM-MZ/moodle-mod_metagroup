5a8e9423b3aff0c67433d8c0c85962b7eb6b96a3
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2021-11-11 12:14:25 +0100

    Correction for Javascript

--- amd/rollup.config.js
+++ amd/rollup.config.js
@@ -1,18 +1,18 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import livereload from 'rollup-plugin-livereload'
-// import { uglify } from 'rollup-plugin-uglify'
+// import livereload from 'rollup-plugin-livereload'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default ({
   input: 'es6/metalink.js',
   output: {
-    file: 'src/metalink.js',
+    file: 'build/metalink.min.js',
     format: 'amd'
   },
   plugins: [
     nodeResolve(),
     commonjs(),
-    // uglify(),
-    livereload()
+    uglify()
+    // livereload()
   ]
 })
--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021110902;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2021111100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020061500;        // Requires this Moodle version
 $plugin->component = 'enrol_metagroup'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 60 * 60;           // Run cron every hour by default, it is not out-of-sync often
