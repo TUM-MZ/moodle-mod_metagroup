e7554d8b635f723bbd70dda3c750390cd0a34f82
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2022-04-07 13:43:07 +0200

    Adding the event listener *after* we replace the element

--- amd/es6/metalink.js
+++ amd/es6/metalink.js
@@ -1,3 +1,5 @@
+/* eslint-env amd */
+
 import debounce from 'lodash/debounce'
 
 let lang = {}
@@ -13,7 +15,10 @@ export const init = function (_lang) {
 
   const link = document.querySelector('#id_link')
   link.outerHTML = `<select class="custom-select" name=${link.name} id="id_link" disabled></select>`
-  link.addEventListener('change', handleLoadGroups)
+  window.requestAnimationFrame(_ => {
+    const link = document.querySelector('#id_link')
+    link.addEventListener('change', handleLoadGroups)
+  })
 
   const group = document.querySelector('#id_group')
   group.outerHTML = `<select class="custom-select" name="${group.name}" id="id_group" disabled></select>`
